e97a3547242adc5c773239437c3dd2838ce023fd
ilya <ilyanosevich@gmail.com>
2024-12-04 12:10:01 +0300

    класс работы с fb2 файлами

---
+++ app/src/main/java/com/example/mybooksapp/Fb2Viewer.kt
@@ -0,0 +1,23 @@
+package com.example.mybooksapp
+
+import com.kursx.parser.fb2.FictionBook
+import org.xml.sax.SAXException
+import java.io.File
+import java.io.IOException
+import javax.xml.parsers.ParserConfigurationException
+
+class Fb2Viewer {
+    fun View() {
+        try {
+            var fb2 = FictionBook(File("book.fb2"))
+        } catch (e: ParserConfigurationException) {
+            e.printStackTrace()
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
+        catch (e: SAXException) {
+            e.printStackTrace()
+        }
+    }
+
+}
